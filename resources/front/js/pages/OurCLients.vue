88bc437e481f03c9430871e47004ffa0c6685f89
Derar sattouf <derar.dev@gmail.com>
2022-08-31 23:55:48 +0300

    test

--- resources/front/js/pages/OurCLients.vue
+++ resources/front/js/pages/OurCLients.vue
@@ -1,4 +1,6 @@
 <template>
+    {{enviro}}
+    {{fb}}
     <div class="container" v-if="enviro">
         <h1 class="text-5xl font-medium mt-20 mb-2"> Our Clients</h1>
         <p class="mb-16 text-xl"> We're proud of helping the best companies deliver positive emotions <br> to their customers.</p>
@@ -17,7 +19,7 @@ import ClientsList from "../components/ClientsList.vue";
 
 const fb = ref()
 const enviro = ref()
-
+const base = import.meta.env.MODE == "development" ? "" : "public/"
 function getProjects() {
     fetch('/api/front/clients')
         .then((response) => response.json())
